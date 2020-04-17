68dc79a284c82668d8ac4ef41c90d99a07883624
John Flinchbaugh <jflinchbaugh@sonatype.com>
2020-04-17 18:32:58 -0400

    INT-2542 rename NXRM kind to NexusRepo

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-kubectl delete -f deploy/crds/apm.nxrm.com_v1alpha1_nxrm_cr.yaml
+kubectl delete -f deploy/crds/apm.nxrm.com_v1alpha1_nexusrepo_cr.yaml
 kubectl delete -f deploy/operator.yaml
-kubectl delete -f deploy/crds/apm.nxrm.com_nxrms_crd.yaml
+kubectl delete -f deploy/crds/apm.nxrm.com_nexusrepos_crd.yaml
 kubectl delete -f deploy/role.yaml
 kubectl delete -f deploy/role_binding.yaml
 kubectl delete -f deploy/service_account.yaml
