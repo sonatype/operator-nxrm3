b1ef0e138d65dcbe12eb4ce23c5b928a0d695260
John Flinchbaugh <jflinchbaugh@sonatype.com>
2022-05-05 18:24:31 -0400

    switch install script from 'create' to 'apply'
    facilitates updates

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-kubectl create -f deploy/service_account.yaml
-kubectl create -f deploy/role.yaml
-kubectl create -f deploy/role_binding.yaml
-kubectl create -f deploy/crds/sonatype.com_nexusrepos_crd.yaml
-kubectl create -f deploy/operator.yaml
-kubectl create -f deploy/crds/sonatype.com_v1alpha1_nexusrepo_cr.yaml
+kubectl apply -f deploy/service_account.yaml
+kubectl apply -f deploy/role.yaml
+kubectl apply -f deploy/role_binding.yaml
+kubectl apply -f deploy/crds/sonatype.com_nexusrepos_crd.yaml
+kubectl apply -f deploy/operator.yaml
+kubectl apply -f deploy/crds/sonatype.com_v1alpha1_nexusrepo_cr.yaml
